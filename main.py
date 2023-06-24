43773358b83f8634963127a5e5789c1c55c3c716
Yashvardhan Baid <codechaser2002@gmail.com>
2023-06-24 17:37:36 +0530

    final integration and bugfixes

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 from sys import platform
 import json
 import os
+from datetime import datetime
 
 from src import WeatherForecastingService
 
 class WeatherForecastingTool:
 
     def __init__(self) -> None:
-        self.city = "Bangalore"
-
-    def __init__(self, city) -> None:
-        self.city = city
+        self._city = "Bangalore"
+        self._weather_forecasting_service = WeatherForecastingService()
 
     def set_city(self, city) -> None:
         """Sets the city to be used for the weather forecasting tool
         """
-        self.city = city
+        self._city = city
+        self._weather_forecasting_service.set_city(city)
         return
 
     def get_city(self) -> str:
         """Returns the currently set city
         """
-        return self.city
+        return self._city
 
-    def print_menu(self) -> None:
+    def _print_menu(self) -> None:
         """Prints the menu for the Weather Forecasting Tool
         """
         # make a menu with the following options:
@@ -37,7 +37,7 @@ class WeatherForecastingTool:
         print("======================================================")
         print("~~~~~~~~~~~ || Weather Forecasting Tool || ~~~~~~~~~~~")
         print("------------------------------------------------------")
-        print("[00] Change City (Current: " + self.city + ")")
+        print("[00] Change City (Current: " + self._city + ")")
         print("[01] Current Weather Forecast")
         print("[02] Today's Weather Forecast")
         print("[03] Tomorrow's Weather Forecast")
@@ -48,24 +48,91 @@ class WeatherForecastingTool:
 
         return
     
-    def print_current(self, current_weather) -> None:
+    def _check_error(self, data_dict) -> bool:
+        """Checks if the data_dict has any error
+        """
+        if str(data_dict["cod"]) != "200":
+            print("Some error occurred.\nPlease check the error message below:")
+            print("Error Code: " + str(data_dict["cod"]))
+            print("Error Message: " + str(data_dict["message"]))
+            return True
+        return False
+    
+    def _print_current(self) -> None:
         """Fetches and prints the current weather forecast
         """
+        data_dict = self._weather_forecasting_service.getCurrent()
+
+        if self._check_error(data_dict):
+            return
+        
+        print("City: " + data_dict["name"])
+        print("Current Time: " + datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+        print("Current Temperature: " + str(data_dict["main"]["temp"]) + "°F")
+        print("Feels Like: " + str(data_dict["main"]["feels_like"]) + "°F")
+        print("Weather: " + data_dict["weather"][0]["main"])
+        print("Weather Description: " + data_dict["weather"][0]["description"])
+
         return
     
-    def print_today(self, today_weather) -> None:
-        """Fetches and prints the today's weather forecast
+    def _print_today(self) -> None:
+        """Fetches and prints the today's weather forecast (3 hour interval)
         """
+        data_dict = self._weather_forecasting_service.getToday()
+
+        if self._check_error(data_dict):
+            return
+        
+        cnt = 8
+
+        for i in range(cnt):
+            print("------------------------------------------------------")
+            print("Time: " + datetime.fromtimestamp(data_dict["list"][i]["dt"]).strftime("%d/%m/%Y %H:%M:%S"))
+            print("Temperature: " + str(data_dict["list"][i]["main"]["temp"]) + "°F")
+            print("Weather: " + data_dict["list"][i]["weather"][0]["main"])
+            print("Weather Description: " + data_dict["list"][i]["weather"][0]["description"])
+            print("------------------------------------------------------")
+
         return
     
-    def print_tomorrow(self, tomorrow_weather) -> None:
+    def _print_tomorrow(self) -> None:
         """Fetches and prints the tomorrow's weather forecast
         """
+        data_dict = self._weather_forecasting_service.getTomorrow()
+
+        if self._check_error(data_dict):
+            return
+        
+        cnt = 8
+
+        for i in range(cnt):
+            print("------------------------------------------------------")
+            print("Time: " + datetime.fromtimestamp(data_dict["list"][i+8]["dt"]).strftime("%d/%m/%Y %H:%M:%S"))
+            print("Temperature: " + str(data_dict["list"][i+8]["main"]["temp"]) + "°F")
+            print("Weather: " + data_dict["list"][i+8]["weather"][0]["main"])
+            print("Weather Description: " + data_dict["list"][i+8]["weather"][0]["description"])
+            print("------------------------------------------------------")
+
         return
     
-    def print_five_day(self, five_day_weather) -> None:
+    def _print_five_day(self) -> None:
         """Fetches and prints the 5 day weather forecast
         """
+        data_dict = self._weather_forecasting_service.getFiveDay()
+
+        if self._check_error(data_dict):
+            return
+
+        cnt = 40
+
+        for i in range(cnt):
+            print("------------------------------------------------------")
+            print("Time: " + datetime.fromtimestamp(data_dict["list"][i]["dt"]).strftime("%d/%m/%Y %H:%M:%S"))
+            print("Temperature: " + str(data_dict["list"][i]["main"]["temp"]) + "°F")
+            print("Weather: " + data_dict["list"][i]["weather"][0]["main"])
+            print("Weather Description: " + data_dict["list"][i]["weather"][0]["description"])
+            print("------------------------------------------------------")
+
         return
     
 
@@ -75,6 +142,7 @@ class WeatherForecastingTool:
         """
         # create a WeatherForecastingTool object
         weather_forecasting_tool = WeatherForecastingTool()
+
         continue_flag = True
 
         while continue_flag:
@@ -86,7 +154,7 @@ class WeatherForecastingTool:
                 os.system("clear")
 
             # print the menu
-            weather_forecasting_tool.print_menu()
+            weather_forecasting_tool._print_menu()
 
             # keep asking for the choice until a valid choice is entered
             invalid_choice = True
@@ -94,33 +162,43 @@ class WeatherForecastingTool:
                 choice = int(input("Enter your choice: "))
                 invalid_choice = False
                 if choice == 0:
-                    city = input("Enter the city: ")
+                    # city name should not start with a number or a special character
+                    invalid_city_name = True
+                    while invalid_city_name:
+                        city = input("Enter the city: ")
+                        if city.isalpha():
+                            invalid_city_name = False
+
                     weather_forecasting_tool.set_city(city)
+                    weather_forecasting_tool._weather_forecasting_service.set_city(city)
+                    
 
                 elif choice == 1:
-                    weather_forecasting_tool.print_current()
+                    weather_forecasting_tool._print_current()
 
                 elif choice == 2:
-                    weather_forecasting_tool.print_today()
+                    weather_forecasting_tool._print_today()
 
                 elif choice == 3:
-                    weather_forecasting_tool.print_tomorrow()
+                    weather_forecasting_tool._print_tomorrow()
 
                 elif choice == 4:
-                    weather_forecasting_tool.print_five_day()
+                    weather_forecasting_tool._print_five_day()
 
                 elif choice == -1:
                     continue_flag = False
+                    continue
 
                 else:
                     print("Invalid choice. Please enter again.")
                     invalid_choice = True
+
+                input("NOTE: Screen will be cleared after this.\nPress Enter to continue...")
             # end of while invalid_choice    
         # end of while continue_flag
 
         return
 
 if __name__ == "__main__":
-    WeatherForecastingService.get_api_key()
     WeatherForecastingTool.start()
     
--- src/weather_forecasting_service.py
+++ src/weather_forecasting_service.py
@@ -12,49 +12,58 @@ class WeatherForecastingService:
         FIVE_DAY = "forecast"
 
     def __init__(self):
+        self._city = "Bangalore"
+
+    def set_city(self, city) -> None:
+        """Sets the city to be used for the weather forecasting tool
+        """
+        self._city = city
         return
     
-    def _build_query(self, city, type) -> str:
-        api_key = self.get_api_key()
-        url_encoded_city_name = city.replace(" ", "+")
+    def get_city(self) -> str:
+        """Returns the currently set city
+        """
+        return self._city
+    
+    def _build_query(self, type) -> str:
+        api_key = self._get_api_key()
+        url_encoded_city_name = self._city.replace(" ", "+")
         units = ""
         url = (
             f"{self._api_base_url}/{type}?q={url_encoded_city_name}"
             f"&units={units}&appid={api_key}"
         )
         return url
-
-    def getCurrent(self, city):
-        type = "weather"
-        url = self._build_query(city, type)
-        json = self._get_weather_data(url)
-        return json
-
-    def getToday(self, city):
-        type = "forecast"
-        url = self._build_query(city, type)
-        json = self._get_weather_data(url)
-        return json
-
-    def getTomorrow(self, city):
-        type = "forecast"
-        url = self._build_query(city, type)
-        json = self._get_weather_data(url)
-        return json
-
-    def getFiveDay(self, city):
-        type = "forecast"
-        url = self._build_query(city, type)
-        json = self._get_weather_data(url)
-        return json
     
-    def get_api_key(self):
+    def _get_api_key(self):
         config = ConfigParser()
-        config.read("../secrets.ini")
-        print(config["openweather"]["api_key"])
+        config.read("./secrets.ini")
         return config["openweather"]["api_key"]
-    
     
     def _get_weather_data(self, query_url):
         response = requests.get(query_url)
         return response.json()
+
+    def getCurrent(self):
+        type = "weather"
+        url = self._build_query(type)
+        json = self._get_weather_data(url)
+        return json
+
+    def getToday(self):
+        type = "forecast"
+        url = self._build_query(type)
+        json = self._get_weather_data(url)
+        return json
+
+    def getTomorrow(self):
+        type = "forecast"
+        url = self._build_query(type)
+        json = self._get_weather_data(url)
+        return json
+
+    def getFiveDay(self):
+        type = "forecast"
+        url = self._build_query(type)
+        json = self._get_weather_data(url)
+        return json
